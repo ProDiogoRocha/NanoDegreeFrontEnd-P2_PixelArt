77979cc92276633af7bc3b7c74fefa4b5fbfd33b
Diogo Rocha <pro.diogorocha@gmail.com>
2018-04-01 21:15:00 -0300

    Inclusão de comentários no código

--- designs.js
+++ designs.js
@@ -1,18 +1,24 @@
+// add event listener Quando o formulário é submetido e chama makeGrid ()
 document.querySelector("#sizePicker").addEventListener("submit", function (e) {
     e.preventDefault();
     makeGrid();
 }, false);
 
 function makeGrid() {
+    // Define a altura e largura de entrada do usuário.
     const pixelHeight = document.querySelector("#input_height").value;
     const pixelWidth = document.querySelector("#input_width").value;
 
+    // Define o elemento da tabela.
     const pixelTable = document.querySelector("#pixel_canvas");
 
+    // Limpe a tabela quando o usuário reenviar a altura e a largura.
+    // https://stackoverflow.com/questions/7271490/delete-all-rows-in-an-html-table#answer-44971572
     while (pixelTable.hasChildNodes()) {
         pixelTable.removeChild(pixelTable.lastChild);
     }
 
+    // Faz um loop dinamicamente para criar a tabela com base na entrada do usuário.
     for (let row = 0; row < pixelHeight; row++) {
         let tr = document.createElement('tr');
         tr.id = 'row-' + row;
@@ -24,7 +30,10 @@ function makeGrid() {
         }
     }
 
+    
+    // Cria novas instâncias de todo o document.querySelectorAll ("td").
     let tableCells = Array.from(document.querySelectorAll("td"));
+    // Cada célula deve ter um ouvinte de eventos que defina a cor do plano de fundo da célula para a cor selecionada.
     tableCells.forEach(function (cell) {
         cell.addEventListener("click", function () {
             this.style.backgroundColor = document.querySelector("#colorPicker").value;
